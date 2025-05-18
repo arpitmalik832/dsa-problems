4789a07f53750231d5d8998624373ccb13786c3d
Arpit Malik <arpit08malik2000@gmail.com>
2025-05-18 19:54:21 +0530

    added java problems

--- problems/scaler/Prefix_sum/Equillibrium_index.js
+++ problems/scaler/Prefix_sum/Equillibrium_index.js
@@ -1,5 +1,12 @@
 /**
  * Equilibrium Index
+ *
+ * Problem Description
+ * Given an array A of N integers.
+ *
+ * Find the equilibrium index of the given array.
+ *
+ * Equilibrium index of an array is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes.
  */
 
 function soln(A) {
@@ -10,13 +17,14 @@ function soln(A) {
 
   if (A[n - 1] - A[0] === 0) return 0;
 
-  if (A[n - 2] === 0) return n - 1;
 
   for (let i = 1; i < n - 1; i++) {
     if (A[i - 1] === A[n - 1] - A[i]) {
       return i;
     }
   }
+
+  if (A[n - 2] === 0) return n - 1;
 
   return -1;
 }
---
+++ src/main/java/com/dsa/problems/prefix_sum/Range_Sum_Query.java
@@ -0,0 +1,39 @@
+package com.dsa.problems.prefix_sum;
+
+import java.util.ArrayList;
+
+public class Range_Sum_Query {
+  public ArrayList<Long> prefixSum(ArrayList<Integer> A) {
+    ArrayList<Long> prefixSum = new ArrayList<>();
+
+    int n = A.size();
+    for (int i = 0; i < n; i++) {
+      if (i == 0) {
+        prefixSum.add((long) A.get(i));
+      } else {
+        prefixSum.add(prefixSum.get(i - 1) + A.get(i));
+      }
+    }
+
+    return prefixSum;
+  }
+
+  public ArrayList<Long> rangeSum(ArrayList<Integer> A, ArrayList<ArrayList<Integer>> B) {
+    int m = B.size();
+    ArrayList<Long> prefixSum = prefixSum(A);
+    ArrayList<Long> rslt = new ArrayList<>();
+
+    for (int i = 0; i < m; i++) {
+      int start = B.get(i).get(0);
+      int end = B.get(i).get(1);
+      if (start == 0) {
+        rslt.add(prefixSum.get(end));
+      } else {
+        rslt.add(prefixSum.get(end) - prefixSum.get(start - 1));
+      }
+    }
+
+    return rslt;
+  }
+
+}
