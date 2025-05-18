c2da500a7cf2889f6e40155c44900c91dd30ecb9
Arpit Malik <arpit08malik2000@gmail.com>
2025-05-18 12:20:24 +0530

    added java setup

---
+++ src/main/java/com/dsa/intro/Data_types.java
@@ -0,0 +1,26 @@
+package com.dsa.intro;
+
+public class Data_types {
+  public static void main(String[] args) {
+    int a = 10;
+    System.out.println(a);
+
+    long b = 10000000;
+    System.out.println(b);
+
+    float c = 10.5f;
+    System.out.println(c);
+
+    double d = 10.5;
+    System.out.println(d);
+
+    char e = 'a';
+    System.out.println(e);
+
+    boolean f = true;
+    System.out.println(f);
+
+    String g = "Hello";
+    System.out.println(g);
+  }
+}
---
+++ src/main/java/com/dsa/intro/Print.java
@@ -0,0 +1,14 @@
+package com.dsa.intro;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.println("Hello, DSA Practice!"); // Hello, DSA Practice!
+        System.out.println("Hey there"); // Hey there
+        System.out.println(10); // 10
+        System.out.println("90 * 50"); // 90 * 50
+        System.out.println(90 * 50); // 4500
+        System.out.println(10 + 20 + 30); // 60
+        System.out.println("10" + 20 + 50); // 102050
+        System.out.println(20 + 50 + "10"); // 7010
+    }
+}
