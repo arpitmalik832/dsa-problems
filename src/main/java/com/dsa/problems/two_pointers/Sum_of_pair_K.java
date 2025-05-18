3f2c18ea1581f3875b5c5340c86a39092227b071
Arpit Malik <arpit08malik2000@gmail.com>
2025-05-19 02:03:40 +0530

    added java problems

---
+++ src/main/java/com/dsa/problems/two_pointers/Sum_of_pair_K.java
@@ -0,0 +1,35 @@
+
+/**
+ * Given A[n] of sorted elements. Check if there exists a pair (i, j) such that
+ * A[i] + A[j] = k and i != j.
+ */
+package com.dsa.problems.two_pointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Sum_of_pair_K {
+  public static boolean checkForSum(ArrayList<Integer> A, int B) {
+    int n = A.size();
+    int i = 0;
+    int j = n - 1;
+    while (i < j) {
+      int sum = A.get(i) + A.get(j);
+      if (sum == B) {
+        return true;
+      } else if (sum < B) {
+        i++;
+      } else {
+        j--;
+      }
+    }
+
+    return false;
+  }
+
+  public static void main(String[] args) {
+    ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+    int B = 5;
+    System.out.println(checkForSum(A, B));
+  }
+}
